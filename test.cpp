ca79bb0cb810d836726e3135ae0781b55c222b80
Zexy1312 <85554366+Zexy1312@users.noreply.github.com>
2021-06-25 18:12:20 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+ string color, flower, pronoun;
+
+ cout << "Enter a color: ";
+ getline(cin, color);
+
+ cout << "Enter a flower: ";
+ getline(cin, flower);
+
+ cout << "Enter a pronoun: ";
+ getline(cin, pronoun);
+
+
+ cout << "Roses are " << color << endl;
+ cout << flower << " are blue " << endl;
+ cout << "All " << pronoun << " to do is fuck you" << endl;
+
+
+
+
+
+    return 0;
+
+}
+
