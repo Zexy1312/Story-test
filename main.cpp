b469de1d45a081a2608107dab7157d7fccf6453c
Zexy1312 <85554366+Zexy1312@users.noreply.github.com>
2021-06-25 18:17:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string name, year, hobbie;
+
+  cout << "Enter your name: ";
+  getline(cin, name);
+
+  cout << "Enter your age: ";
+  getline(cin, year);
+
+  cout << "Enter your hobbie: ";
+  getline(cin, hobbie);
+
+
+  cout << "Hello I am " << name << endl;
+  cout << "I am " << year << " years old " << endl;
+  cout << "On my free time i love to " << hobbie << endl;
+  cout << "Also ncploufer is a bitch " << endl;
+
+
+
+    return 0;
+
+
+}
+
